b5da58db14961e7867c8b44a9ebaedce12dbc80b
Carlos Vela Chávez <113889326+cvelcha3@users.noreply.github.com>
2023-02-04 19:18:21 +0100

    fuegos hecho

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -7,16 +7,30 @@ public class Fire : MonoBehaviour
 {
     private Light2D lighting;
     private ColorPuzzle puzzle;
+    private SpriteRenderer spriteFire;
+    private bool spriteDimming = false;
     private void Start()
     {
+        spriteFire = GetComponent<SpriteRenderer>();
         lighting = GetComponent<Light2D>();
         puzzle = transform.parent.GetComponent<ColorPuzzle>();
+        spriteFire.color = lighting.color;
     }
     void OnMouseOver()
     {
-        if (Input.GetButtonDown("Fire1") && !puzzle.isDone){
+        if (Input.GetButtonDown("Fire1") && !puzzle.isDone)
+        {
             lighting.enabled = !lighting.enabled;
             puzzle.CheckLights();
+            spriteDimming = !spriteDimming;
+            if (spriteDimming)
+            {
+                spriteFire.color = new Color(lighting.color.r, lighting.color.g, lighting.color.b, 0.10f);
+            }
+            else
+            {
+                spriteFire.color = new Color(lighting.color.r, lighting.color.g, lighting.color.b, 0.75f);
+            }
         }
     }
 }
