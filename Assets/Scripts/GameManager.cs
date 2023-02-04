1d160de09bc6dbbf7a47e46fb14edc14de35eda6
cvelcha3 <cvela@iesrafaelalberti.es>
2023-02-04 22:58:06 +0100

    hint system done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,10 @@ public class GameManager : MonoBehaviour
     [SerializeField] List<Sprite> backgrounds;
     [SerializeField] List<int> puzzleIndex;
     [SerializeField] List<GameObject> puzzleObjects;
+    [SerializeField] List<string> hintsTexts;
     [SerializeField] Image background;
+    [SerializeField] GameObject hintIcon;
+    [SerializeField] TextMeshProUGUI hints;
 
     private static GameManager instance;
     private int index = 0;
@@ -38,6 +42,9 @@ public class GameManager : MonoBehaviour
         background.sprite = backgrounds[this.index];
         if(this.index == puzzleIndex[thisPuzzle])
         {
+            hintIcon.SetActive(true);
+            hints.gameObject.transform.parent.gameObject.SetActive(false);
+            hints.text = hintsTexts[thisPuzzle];
             puzzleObjects[thisPuzzle].SetActive(true);
             thisPuzzle += 1;
             this.index += 1;
